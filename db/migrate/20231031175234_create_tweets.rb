2ac435dd96527339d84fd503c4c3778324ce0e73
Ibuki Takazawa <2111140011u@ed.fuk.kindai.ac.jp>
2023-11-02 06:17:05 +0000

    push to render.com

--- db/migrate/20231031175234_create_tweets.rb
+++ db/migrate/20231031175234_create_tweets.rb
@@ -1,10 +1,12 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
-    create_table :tweets do |t|
-      t.string :message
-      t.integer :user_id
-
-      t.timestamps
+    unless ActiveRecord::Base.connection.table_exists?('tweets')
+      create_table :tweets do |t|
+        t.string :message
+        t.integer :user_id
+  
+        t.timestamps
+      end
     end
   end
 end
