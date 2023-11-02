db8871e64e9a2d0549c33409a0d2033391120f15
Ibuki Takazawa <2111140011u@ed.fuk.kindai.ac.jp>
2023-11-02 06:14:56 +0000

    push to render.com

--- db/migrate/20231031175150_create_users.rb
+++ db/migrate/20231031175150_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
-      t.string :uid
-      t.string :pass
-
-      t.timestamps
+    unless ActiveRecord::Base.connection.table_exists?('users')
+      create_table :users do |t|
+        t.string :uid
+        t.string :pass
+  
+        t.timestamps
+      end
     end
   end
 end
-
